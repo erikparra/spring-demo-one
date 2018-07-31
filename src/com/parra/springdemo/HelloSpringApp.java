7c7fbb28e77fff76bd536e3986fdf7c83c824e93
Erik Parra <erik.parra4@gmail.com>
2018-07-30 23:06:05 -0500

    Added Config, Using Spring Bean

---
+++ src/com/parra/springdemo/HelloSpringApp.java
@@ -0,0 +1,21 @@
+package com.parra.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve a bean from the spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		// call a method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close application context
+		context.close();
+	}
+
+}
