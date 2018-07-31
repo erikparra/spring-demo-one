129824cdd5ab8cb94cc638bd4d50179833ff93da
Erik Parra <erik.parra4@gmail.com>
2018-07-30 23:12:35 -0500

    Added PingpongCoach class

---
+++ src/com/parra/springdemo/PingpongCoach.java
@@ -0,0 +1,10 @@
+package com.parra.springdemo;
+
+public class PingpongCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 44 minutes practicing on table.";
+	}
+	
+}
