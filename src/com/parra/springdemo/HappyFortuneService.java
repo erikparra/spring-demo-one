bdb157cc542e3b4104a98d78fc6f02bf485cd4ca
Erik Parra <erik.parra4@gmail.com>
2018-07-31 21:50:40 -0500

    Added random fortune to fortune interface

--- src/com/parra/springdemo/HappyFortuneService.java
+++ src/com/parra/springdemo/HappyFortuneService.java
@@ -1,10 +1,18 @@
 package com.parra.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
+	private String[] fortuneArray = {"He who sits on toliet is high on pot",
+	                                           	"Today is your day!",
+	                                           	"Today is NOT your day!",
+	                                           	"You will accomplish your goals"};
 	@Override
 	public String getFortune() {
-		return "He who sits on toliet is high on pot";
+		Random rand = new Random();
+		int index = rand.nextInt(fortuneArray.length);
+		return fortuneArray[index];
 	}
 
 }
