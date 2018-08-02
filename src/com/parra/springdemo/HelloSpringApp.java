8a65994f1c2d4cd84e639cfcc0cd145801f7ca57
Erik Parra <erik.parra4@gmail.com>
2018-08-01 23:26:36 -0500

    Testing prototype scope in applicationContext.xml

--- src/com/parra/springdemo/HelloSpringApp.java
+++ src/com/parra/springdemo/HelloSpringApp.java
@@ -10,11 +10,15 @@ public class HelloSpringApp {
 		
 		//retrieve a bean from the spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach theCoach2 = context.getBean("myCoach", Coach.class);
+		
+		boolean result = (theCoach == theCoach2);
 		
 		// call a method on bean
 		System.out.println(theCoach.getDailyWorkout());
-		
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println("2 Coaches are the same: "+ result);
 		
 		// close application context
 		context.close();
